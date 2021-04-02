d2984daeedef246abf8b0ab86d95c13c21b30db9
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-04-02 16:11:03 -0300

    Projeto Teste Boa tarde

--- ProjetoGit/Program.cs
+++ ProjetoGit/Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Evening!");
             Console.ReadLine();
         }
     }
